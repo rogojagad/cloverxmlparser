0f070d86fe12db7e3c843592321097d39d583e52
samuellusandi <samuel.lusandi@gdplabs.id>
2019-10-25 13:16:19 +0700

    Add Testdox Analyzer

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,65 @@
 <template>
-  <router-view/>
+  <div>
+    <b-navbar shadow>
+      <template slot="start">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">Coverage Parser</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/testdox' }">Testdox Parser</b-navbar-item>
+      </template>
+    </b-navbar>
+    <router-view class="content"/>
+  </div>
 </template>
+<script>
+export default {
+
+}
+</script>
+<style>
+.content {
+  padding-top: 1.5%;
+}
+
+.container {
+  margin-top: 2%;
+}
+
+.health-skipped {
+  background: grey;
+  color: black;
+}
+
+.health-dangerous {
+  background: #b50000;
+  color: white;
+}
+
+.health-warning {
+  background: yellow;
+  color: black;
+}
+
+.health-decent {
+  background: #beff4f;
+  color: black;
+}
+
+.health-good {
+  background: #00ff00;
+  color: black;
+}
+
+.health-perfect {
+  background: #00ff00;
+  color: black;
+  font-weight: bold;
+}
+
+.centerbold {
+  font-weight: bolder;
+  text-align: center;
+}
+
+b-table-column {
+  word-wrap: break-word;
+}
+</style>
